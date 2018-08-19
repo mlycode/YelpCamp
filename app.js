89980a771e713a8b70efade85267c6707b72b976
Michael Ly <m.ly@live.co.uk>
2018-08-19 14:27:22 +0000

    add mongolab

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes = require("./routes/comments"),
     
 //APP CONFIG
 //seedDB();
-mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true }); 
+//mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true }); //Local DB
+mongoose.connect("mongodb://mlycode:password123@ds123852.mlab.com:23852/yelpcampmly", { useNewUrlParser: true }); //MLab hosted DB
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
